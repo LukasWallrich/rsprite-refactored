0c086a16fc8c1700c8f9e88d74c7e962bc3144c7
Lukas Wallrich <lukas.wallrich@gmail.com>
2021-08-18 20:27:50 +0100

    Install packages as required

--- libraries.R
+++ libraries.R
@@ -1,5 +1,20 @@
 #!/usr/bin/env Rscript
 
+required_packages <- c("ggplot2", "gridExtra", "moments", "shiny", "shinycssloaders")
+res <- unlist(suppressWarnings(lapply(required_packages, requireNamespace, quietly = TRUE)))
+if (!all(res)) {
+  warning(paste0(note, "The following packages are required for rSPRITE but
+                   cannot be loaded: ", paste0(required_packages[!res], collapse = ", ")),
+    call. = FALSE
+  )
+  choice <- readline(prompt = "Should I try to install these packages? (Y/N)")
+  if (tolower(substr(choice, 1, 1)) == "y") {
+    utils::install.packages(x[!res])
+  } else {
+    stop("Cannot proceed without these packages.", call. = FALSE)
+  }
+}
+
 library(ggplot2)
 library(gridExtra)
 library(moments)
